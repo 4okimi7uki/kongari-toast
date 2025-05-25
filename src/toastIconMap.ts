0f260518a6daa98523856fc629fa004433040dcb
4okimi7uki <bv16001@shibaura-it.ac.jp>
2025-05-25 15:04:22 +0900

    Style: add dark mode

--- src/toastIconMap.ts
+++ src/toastIconMap.ts
@@ -8,7 +8,7 @@ export type ToastType = "default" | "success" | "error" | "warning" | "info" | "
 export const toastIconMap: Record<ToastType, ToastIcon> = {
     promise: {
         color: "",
-        svg: `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_ajPY{transform-origin:center;animation:spinner_AtaB .75s infinite linear}@keyframes spinner_AtaB{100%{transform:rotate(360deg)}}</style><path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25"/><path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" class="spinner_ajPY"/></svg>`,
+        svg: `<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><style>.spinner_ajPY{transform-origin:center;animation:spinner_AtaB .75s infinite linear}@keyframes spinner_AtaB{100%{transform:rotate(360deg)}}</style><path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25"/><path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" class="spinner_ajPY"/></svg>`,
     },
     success: {
         color: "#10b27f",
@@ -35,7 +35,7 @@ export const toastIconMap: Record<ToastType, ToastIcon> = {
             </svg>`,
     },
     default: {
-        color: "linear-gradient(to right, #eef947, #5ac8fa, #007aff, #34aadc, #5856d6, #ff2d55)",
+        color: "linear-gradient(to right,rgb(251, 248, 227), #ffc785, #ff9f68,rgb(255, 145, 77),rgb(234, 83, 96))",
         svg: "",
     },
 };
